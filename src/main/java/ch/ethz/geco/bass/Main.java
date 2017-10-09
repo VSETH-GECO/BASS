b3d24b2bf226ba03f22d741fe3ed88ad9a695e38
David Enderlin <davue@bluewin.ch>
2017-10-09 18:27:56 +0200

    Added logging and updated gitignore

--- src/main/java/ch/ethz/geco/bass/Main.java
+++ src/main/java/ch/ethz/geco/bass/Main.java
@@ -1,12 +1,20 @@
 package ch.ethz.geco.bass;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 
 public class Main {
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+
     private static Server server;
     static Player player;
 
     public static void main(String[] args) throws IOException {
+        logger.info("BASS");
+        logger.info("The GECO Byro Audio Speaker System. Copyright (c) 2017, Licensed under MIT");
+
         YoutubeDL yt = new YoutubeDL();
         switch (yt.checkInstall()) {
             case 0: break;
--- src/main/java/ch/ethz/geco/bass/Server.java
+++ src/main/java/ch/ethz/geco/bass/Server.java
@@ -3,17 +3,20 @@ package ch.ethz.geco.bass;
 import org.java_websocket.WebSocket;
 import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.InetSocketAddress;
 
 /**
  * Server class
- *
+ * <p>
  * Somewhen in the future it should handle all kinds of api
  * requests to modify the queue. Maybe even provide a web-
  * interface.
  */
 public class Server extends WebSocketServer {
+    private Logger logger = LoggerFactory.getLogger(Server.class);
     Player player;
 
     public Server(Player player, int port) {
@@ -23,19 +26,26 @@ public class Server extends WebSocketServer {
 
     @Override
     public void onOpen(WebSocket webSocket, ClientHandshake clientHandshake) {
+        logger.info(webSocket.getRemoteSocketAddress().getHostString() + " connected!");
         webSocket.send("Welcome to BASS");
     }
 
     @Override
     public void onClose(WebSocket webSocket, int i, String s, boolean b) {
+        logger.info(webSocket.getRemoteSocketAddress().getHostString() + " disconnected!");
     }
 
     @Override
     public void onMessage(WebSocket webSocket, String msg) {
+        logger.debug("Message from " + webSocket.getRemoteSocketAddress().getHostString() + ":" + msg);
         String[] args = msg.split(",");
-        switch(args[0]) {
-            case "GET": handleGet(webSocket); break;
-            case "POST": handlePost(webSocket, args[1]); break;
+        switch (args[0]) {
+            case "GET":
+                handleGet(webSocket);
+                break;
+            case "POST":
+                handlePost(webSocket, args[1]);
+                break;
             default:
         }
     }
@@ -65,12 +75,12 @@ public class Server extends WebSocketServer {
 
     @Override
     public void onError(WebSocket webSocket, Exception e) {
-
+        logger.error(e.getMessage());
     }
 
     @Override
     public void onStart() {
-
+        logger.info("WS Server started!");
     }
 
     // TODO maybe useful
