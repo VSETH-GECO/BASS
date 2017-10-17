644abd12102cf989dc056f548d735a669e6bb9bd
David Enderlin <davue@bluewin.ch>
2017-10-17 19:24:54 +0200

    Tried to simplify AudioConsumer
    - Added debug logging
    - Added explanations

--- src/main/java/ch/ethz/geco/bass/audio/AudioConsumer.java
+++ src/main/java/ch/ethz/geco/bass/audio/AudioConsumer.java
@@ -5,6 +5,7 @@ import com.sedmelluq.discord.lavaplayer.format.AudioPlayerInputStream;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import sun.plugin2.jvm.CircularByteBuffer;
 
 import javax.sound.sampled.*;
 import java.io.IOException;
@@ -21,7 +22,7 @@ public class AudioConsumer extends Thread {
     /**
      * The output format
      */
-    public static final AudioDataFormat outputFormat = new AudioDataFormat(2, 44100, 960, AudioDataFormat.Codec.PCM_S16_LE);
+    public static final AudioDataFormat outputFormat = new AudioDataFormat(2, 48000, 960, AudioDataFormat.Codec.PCM_S16_LE);
 
     /**
      * The audio output line
@@ -38,25 +39,38 @@ public class AudioConsumer extends Thread {
 
             output.start();
 
-            byte[] buffer = new byte[outputFormat.bufferSize(8)];
+            byte[] buffer = new byte[outputFormat.bufferSize(2)];
             int chunkSize;
+
+            logger.debug("Audio Format:");
+            logger.debug(String.format("%-25s | %-25s | %-25s", "Codec: " + outputFormat.codec.name(), "Channels: " + outputFormat.channelCount, "Sample Rate: " + outputFormat.sampleRate + " [Samples/s]"));
+            logger.debug(String.format("%-25s | %-25s | %-25s", "Frame Duration: " + outputFormat.frameDuration() + " [ms]", "Frame Size: " + outputFormat.chunkSampleCount + " [Samples]", "Output Buffer Size: " + output.getBufferSize() + " [Bytes]"));
 
             logger.info("Started AudioConsumer!");
             try {
                 long frameDuration = outputFormat.frameDuration();
                 AudioPlayer player = AudioManager.getPlayer();
+
+                // We write as much data as we can to the audio output. This will completely fill it's buffer which
+                // contains approximately 1 second of audio data.
                 while (true) {
                     if (!player.isPaused()) {
                         if ((chunkSize = stream.read(buffer)) >= 0) {
+                            // If the buffer is full, this will simply block
                             output.write(buffer, 0, chunkSize);
                         } else {
                             logger.error("Reached end of stream, this should not happen!");
                             logger.error("Stopped AudioConsumer!");
                             break;
                         }
-                    }
+                    } else {
+                        // Drain the rest of the buffer
+                        output.drain();
 
-                    sleep(frameDuration); // Back-off for one frame
+                        // Back-off for a frame to prevent unnecessary usage of cpu time even when the thread has
+                        // to wait for an unpause of the player.
+                        sleep(frameDuration);
+                    }
                 }
             } catch (IOException | InterruptedException e) {
                 e.printStackTrace();
