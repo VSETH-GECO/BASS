1b8692948675a0f33cb99fdd30b40ddfe40462de
Bermos <matthew.machivenyika@hispeed.ch>
2017-11-08 20:46:00 +0100

    Added Favorite Track class for easier serialisation

---
+++ src/main/java/ch/ethz/geco/bass/server/util/FavoriteTrack.java
@@ -0,0 +1,11 @@
+package ch.ethz.geco.bass.server.util;
+
+public class FavoriteTrack {
+    private String uri;
+    private String title;
+
+    public FavoriteTrack(String uri, String title) {
+        this.uri = uri;
+        this.title = title;
+    }
+}
